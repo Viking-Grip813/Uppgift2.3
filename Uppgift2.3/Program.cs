079b9792189c0fe166f0ad5a93e513b926aac642
vist07001 <vist07001@E-PW05CGR8>
2024-10-10 12:19:33 +0200

    Add project files.

---
+++ Uppgift2.3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace uppgift2_3
+{ class Program
+    { static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många dagar ska du köra denna bil");
+            string dagar  =Console.ReadLine();
+           Console.WriteLine("Hur många kilometer ska du köra");
+            int tal = int.Parse(dagar);
+            string kilometer = Console.ReadLine();
+            int tal2 = int.Parse(kilometer);
+            int bannan = tal * 500 + tal2 * 1 + 300;
+            Console.WriteLine("Det kommer att kosta " + bannan);
+        }
+    }
+}
